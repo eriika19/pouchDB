eaaf4e3659b41e8ef856ce6dd9870f78cb54fdc4
eriika19 <51388362+eriika19@users.noreply.github.com>
2019-08-05 15:12:55 -0500

    se personalizo para cuento ibmcloud propia

--- js/server.js
+++ js/server.js
@@ -2,7 +2,7 @@ console.log("connected");
 
 
 
-dbLABORATORIA = new PouchDB('https://c5f0d712-dcca-4c03-9ad2-79f4002de1a8-bluemix:9c85601cb39598bca3d063571301d5c26dea0d35b74c6a6ccb853eae1614a7a4@c5f0d712-dcca-4c03-9ad2-79f4002de1a8-bluemix.cloudant.com/test2');
+dbLABORATORIA = new PouchDB('1c944a9e-45ef-40c4-a388-7412a0b4462d-bluemix:de0bdfd4bf2bfad8b0752ce3fc5f19cf6c65a20689273455cc13cd2e257ab2ea@https://1c944a9e-45ef-40c4-a388-7412a0b4462d-bluemix:de0bdfd4bf2bfad8b0752ce3fc5f19cf6c65a20689273455cc13cd2e257ab2ea@1c944a9e-45ef-40c4-a388-7412a0b4462d-bluemix.cloudantnosqldb.appdomain.cloud/test2');
 
 
 function read(){
@@ -11,7 +11,7 @@ dbLABORATORIA.find({
     sort: ['_id']
   }).then(function (result) {
     console.log(result['docs']);
-    document.getElementById("value").innerHTML= result['docs'][0]['Tema']
+    document.getElementById("value").innerHTML= result['docs'][5]['municipio']
   });
 };
 
